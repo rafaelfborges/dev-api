b3fb8f8f9b7972ee230dba449142212db1db0054
Rafael Borges <rafaelfborges@gmail.com>
2019-12-26 10:21:49 -0300

    Migrando dev-api REST para RESTful

--- app_restful.py
+++ app_restful.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, json
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
@@ -13,11 +13,29 @@ desenvolvedores = [
     }
 ]
 
-class HelloWorld(Resource):
-    def get(self):
-        return {'hello': 'world'}
+class Desenvolvedor(Resource):
+    def get(self, id):
+        try:
+            response = desenvolvedores[id]
+        except IndexError:
+            mensagem = 'Desenvolvedor ID {}, não existe.'.format(id)
+            response = {'status': 'erro', 'mensagem': mensagem}
+        except Exception:
+            mensagem = 'Erro desconhecido. Procure o administrador da API'
+            response = {'status': 'erro', 'mensagem': mensagem}
+        return response
 
-api.add_resource(HelloWorld, '/')
+    def put(self, id):
+        dados = json.loads(request.data)
+        desenvolvedores[id] = dados
+        return dados
+
+    def delete(self, id):
+        desenvolvedores.pop(id)
+        return {'status': 'sucesso', 'mensagem': 'Registro excluído'}
+
+api.add_resource(Desenvolvedor, '/dev/<int:id>/')
+
 
 if __name__ == '__main__':
     app.run()
